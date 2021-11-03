494ab6fcfb5f4b18b048bb1fe9510c6989a8e5cf
dpapanicker <70652939+dpapanicker@users.noreply.github.com>
2021-11-03 18:10:52 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,70 @@
+var towerImg, tower;
+var doorImg, door, doorsGroup;
+var climberImg, climber, climbersGroup;
+var ghost, ghostImg;
+var invisibleBlockGroup, invisibleBlock;
+var gameState = "play"
+
+function preload(){
+  towerImg = loadImage("tower.png");
+  doorImg = loadImage("door.png");
+  climberImg = loadImage("climber.png");
+  ghostImg = loadImage("ghost-standing.png");
+  spookySound = loadSound("spooky.wav");
+}
+
+function setup(){
+  createCanvas(600,600);
+  spookySound.loop();
+  tower = createSprite(300,300);
+  tower.addImage("tower",towerImg);
+  tower.velocityY = 1;
+  
+  doorsGroup = new Group();
+  climbersGroup = new Group();
+  invisibleBlockGroup = new Group();
+  
+  ghost = createSprite(200,200,50,50);
+  ghost.scale = 0.3;
+  ghost.addImage("ghost", ghostImg);
+}
+
+function draw(){
+  background(0);
+  if (gameState === "play") {
+   
+
+    
+    
+    drawSprites();
+  }
+  
+  if (gameState === "end"){
+    stroke("yellow");
+    fill("yellow");
+    textSize(30);
+    text("Game Over", 230,250)
+  }
+
+}
+
+function spawnDoors() {
+  //write code here to spawn the doors in the tower
+  
+
+  
+   
+    //assign lifetime to the variable
+    door.lifetime = 800;
+    climber.lifetime = 800;
+    invisibleBlock.lifetime = 800;
+
+    
+    //add each door to the group
+    doorsGroup.add(door);
+    invisibleBlock.debug = true;
+    climbersGroup.add(climber);
+    invisibleBlockGroup.add(invisibleBlock);
+  }
+}
+
